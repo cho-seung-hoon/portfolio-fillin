435c84b6aedf9ebae96592bee49b174b819246fd
woominp <245330617+woominp@users.noreply.github.com>
2026-01-08 17:24:30 +0900

    /api/v1/lessons/{lessonId}/reviews에 관련된 repository 기능 추가
    lesson에 관한 모든 리뷰 및 작성자 닉네임 가져오기
    lesson의 평점 가져오기
    lesson의 총 리뷰 개수 가져오기

--- src/main/java/com/kosa/fillinv/review/repository/ReviewRepository.java
+++ src/main/java/com/kosa/fillinv/review/repository/ReviewRepository.java
@@ -1,9 +1,24 @@
 package com.kosa.fillinv.review.repository;
 
+import com.kosa.fillinv.review.dto.ReviewWithNicknameVO;
 import com.kosa.fillinv.review.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, String> {
+
+    @Query("SELECT AVG(r.score) FROM Review r WHERE r.lessonId = :lessonId")
+    Double findAverageScoreByLessonId(@Param("lessonId") String lessonId);
+
+    @Query("SELECT new com.kosa.fillinv.review.dto.ReviewWithNicknameVO(r, m.nickname) " +
+            "FROM Review r JOIN Member m ON r.writerId = m.id " +
+            "WHERE r.lessonId = :lessonId")
+    Page<ReviewWithNicknameVO> findReviewsWithNicknameByLessonId(@Param("lessonId") String lessonId, Pageable pageable);
+
+    long countByLessonId(String lessonId);
 }
