e928369d027804a61356b97567c5fc7c849dba30
Jeonghwan Kim <51739985+hwan20202@users.noreply.github.com>
2026-01-09 18:15:14 +0900

    feat : Lesson 등록 컨트롤러 구현

--- src/main/java/com/kosa/fillinv/lesson/controller/LessonController.java
+++ src/main/java/com/kosa/fillinv/lesson/controller/LessonController.java
@@ -1,11 +1,34 @@
 package com.kosa.fillinv.lesson.controller;
 
+import com.kosa.fillinv.global.response.SuccessResponse;
+import com.kosa.fillinv.lesson.controller.dto.RegisterLessonRequest;
+import com.kosa.fillinv.lesson.controller.dto.RegisterLessonResponse;
+import com.kosa.fillinv.lesson.service.LessonRegisterService;
+import com.kosa.fillinv.lesson.service.dto.CreateLessonResult;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/lesson")
+@RequestMapping("/api/v1/lessons")
 public class LessonController {
+
+    private final LessonRegisterService lessonRegisterService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public SuccessResponse<RegisterLessonResponse> registerLesson(
+            @RequestPart("request") RegisterLessonRequest request,
+            @RequestPart(value = "thumbnail", required = false) MultipartFile thumbnail
+    ) {
+        CreateLessonResult createLessonResult = lessonRegisterService.registerLesson(RegisterLessonRequest.toCommand(request), thumbnail);
+
+        return SuccessResponse.success(HttpStatus.OK, RegisterLessonResponse.of(createLessonResult));
+    }
+
 }
---
+++ src/main/java/com/kosa/fillinv/lesson/controller/dto/RegisterLessonRequest.java
@@ -0,0 +1,47 @@
+package com.kosa.fillinv.lesson.controller.dto;
+
+import com.kosa.fillinv.lesson.service.dto.RegisterLessonCommand;
+
+import java.time.Instant;
+import java.util.List;
+
+public record RegisterLessonRequest(
+        String title,
+        String lessonType,
+        String description,
+        String location,
+        String mentorId,
+        Long categoryId,
+        Instant closeAt,
+        Integer price,
+        List<Option> optionList,
+        List<AvailableTime> availableTimeList
+) {
+
+    public static RegisterLessonCommand toCommand(RegisterLessonRequest request) {
+        return new RegisterLessonCommand(
+                request.title,
+                request.lessonType,
+                request.description,
+                request.location,
+                request.mentorId,
+                request.categoryId,
+                request.closeAt,
+                request.price,
+                request.optionList.stream().map(Option::toCommand).toList(),
+                request.availableTimeList.stream().map(at -> new RegisterLessonCommand.AvailableTime(at.startTime, at.endTime, at.price)).toList()
+        );
+    }
+
+    public record Option(String name, Integer minute, Integer price) {
+        public static RegisterLessonCommand.Option toCommand(Option option) {
+            return new RegisterLessonCommand.Option(option.name, option.minute, option.price);
+        }
+    }
+
+    public record AvailableTime(Instant startTime, Instant endTime, Integer price) {
+        public static RegisterLessonCommand.AvailableTime toCommand(AvailableTime availableTime) {
+            return new RegisterLessonCommand.AvailableTime(availableTime.startTime, availableTime.endTime, availableTime.price);
+        }
+    }
+}
