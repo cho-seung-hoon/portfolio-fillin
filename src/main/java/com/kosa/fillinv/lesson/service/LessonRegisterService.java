d95fe1de3988d34548f54d1e1c38c6eda234345c
Jeonghwan Kim <51739985+hwan20202@users.noreply.github.com>
2026-01-09 18:41:34 +0900

    fix : LessonRegisterService file이 없을 경우 처리

--- src/main/java/com/kosa/fillinv/lesson/service/LessonRegisterService.java
+++ src/main/java/com/kosa/fillinv/lesson/service/LessonRegisterService.java
@@ -1,13 +1,17 @@
 package com.kosa.fillinv.lesson.service;
 
+import com.kosa.fillinv.global.exception.ResourceException;
 import com.kosa.fillinv.global.util.FileStorage;
 import com.kosa.fillinv.global.util.UploadFileResult;
+import com.kosa.fillinv.lesson.error.LessonError;
 import com.kosa.fillinv.lesson.service.dto.CreateLessonCommand;
 import com.kosa.fillinv.lesson.service.dto.CreateLessonResult;
 import com.kosa.fillinv.lesson.service.dto.RegisterLessonCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+
+import static com.kosa.fillinv.lesson.error.LessonError.*;
 
 @Component
 @RequiredArgsConstructor
@@ -17,6 +21,9 @@ public class LessonRegisterService {
     private final FileStorage fileStorage;
 
     public CreateLessonResult registerLesson(RegisterLessonCommand command, MultipartFile file) {
+        if (file == null || file.isEmpty()) {
+            throw new ResourceException.InvalidArgument(THUMBNAIL_IMAGE_REQUIRED);
+        }
 
         UploadFileResult upload = fileStorage.upload(file);
 
