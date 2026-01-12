453d4bf7fbd00e80e4dccfd1038faa7bd5d19b4c
woominp <245330617+woominp@users.noreply.github.com>
2026-01-12 15:05:55 +0900

    안쓰는 import 제거

--- src/main/java/com/kosa/fillinv/review/controller/ReviewController.java
+++ src/main/java/com/kosa/fillinv/review/controller/ReviewController.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
