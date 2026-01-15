e915bb82c707c151c28027941cdfe2e580c73377
minwooch <79406120+minwooch@users.noreply.github.com>
2026-01-15 15:55:32 +0900

    apply UI 및 api 데이터 추가

---
+++ src/types/service-application-data.ts
@@ -0,0 +1,47 @@
+export interface SuccessResponse<T> {
+    data: T;
+    status: number;
+    message: string;
+}
+
+export interface LessonDetailResult {
+    mentor: Mentor;
+    lesson: Lesson;
+    options: Option[];
+    availableTimes: AvailableTime[];
+}
+
+export interface Mentor {
+    mentorId: string;
+    nickname: string;
+    profileImage: string;
+    introduction: string;
+}
+
+export interface Lesson {
+    lessonId: string;
+    description: string;
+    lessonType: string;
+    title: string;
+    thumbnailImage: string;
+    price: number;
+    seats: number | null;
+    remainSeats: number | null;
+    categoryId: number;
+}
+
+export interface Option {
+    optionId: string;
+    name: string;
+    minute: number;
+    price: number;
+}
+
+export interface AvailableTime {
+    availableTimeId: string;
+    startTime: string; // ISO 8601 string
+    endTime: string; // ISO 8601 string
+    price: number;
+    seats: number | null;
+    remainSeats: number | null;
+}
